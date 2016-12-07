a3c0c866333f8900516f5f197d7e05a7cecbadab
Antonio <antonio75@gmail.com>
2016-12-07 21:27:05 +0100

    Add project files.

---
+++ WebFilesDownload/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WebFilesDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txbUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.txbFolder = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rdbPdf = new System.Windows.Forms.RadioButton();
+            this.rdbPub = new System.Windows.Forms.RadioButton();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.chklstFiles = new System.Windows.Forms.CheckedListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTotalFiles = new System.Windows.Forms.Label();
+            this.lblTotalDownloaded = new System.Windows.Forms.Label();
+            this.rdbMp3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // txbUrl
+            // 
+            this.txbUrl.Location = new System.Drawing.Point(49, 45);
+            this.txbUrl.Name = "txbUrl";
+            this.txbUrl.Size = new System.Drawing.Size(322, 20);
+            this.txbUrl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(378, 43);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(364, 267);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(89, 30);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txbFolder
+            // 
+            this.txbFolder.Location = new System.Drawing.Point(57, 273);
+            this.txbFolder.Name = "txbFolder";
+            this.txbFolder.Size = new System.Drawing.Size(254, 20);
+            this.txbFolder.TabIndex = 5;
+            this.txbFolder.Click += new System.EventHandler(this.txbFolder_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Folder";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "File type:";
+            // 
+            // rdbPdf
+            // 
+            this.rdbPdf.AutoSize = true;
+            this.rdbPdf.Checked = true;
+            this.rdbPdf.Location = new System.Drawing.Point(68, 20);
+            this.rdbPdf.Name = "rdbPdf";
+            this.rdbPdf.Size = new System.Drawing.Size(41, 17);
+            this.rdbPdf.TabIndex = 8;
+            this.rdbPdf.TabStop = true;
+            this.rdbPdf.Text = "Pdf";
+            this.rdbPdf.UseVisualStyleBackColor = true;
+            this.rdbPdf.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rdbPub
+            // 
+            this.rdbPub.AutoSize = true;
+            this.rdbPub.Location = new System.Drawing.Point(119, 20);
+            this.rdbPub.Name = "rdbPub";
+            this.rdbPub.Size = new System.Drawing.Size(50, 17);
+            this.rdbPub.TabIndex = 9;
+            this.rdbPub.Text = "ePub";
+            this.rdbPub.UseVisualStyleBackColor = true;
+            // 
+            // chklstFiles
+            // 
+            this.chklstFiles.FormattingEnabled = true;
+            this.chklstFiles.Location = new System.Drawing.Point(18, 91);
+            this.chklstFiles.Name = "chklstFiles";
+            this.chklstFiles.Size = new System.Drawing.Size(435, 169);
+            this.chklstFiles.TabIndex = 10;
+            this.chklstFiles.SelectedIndexChanged += new System.EventHandler(this.chklstFiles_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(18, 346);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(435, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(247, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Uncheck the files that you don\'t want to download.";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(18, 321);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(72, 13);
+            this.lblStatus.TabIndex = 13;
+            this.lblStatus.Text = "Downloading:";
+            // 
+            // lblTotalFiles
+            // 
+            this.lblTotalFiles.AutoSize = true;
+            this.lblTotalFiles.Location = new System.Drawing.Point(352, 321);
+            this.lblTotalFiles.Name = "lblTotalFiles";
+            this.lblTotalFiles.Size = new System.Drawing.Size(55, 13);
+            this.lblTotalFiles.TabIndex = 14;
+            this.lblTotalFiles.Text = "Total files:";
+            // 
+            // lblTotalDownloaded
+            // 
+            this.lblTotalDownloaded.AutoSize = true;
+            this.lblTotalDownloaded.Location = new System.Drawing.Point(186, 321);
+            this.lblTotalDownloaded.Name = "lblTotalDownloaded";
+            this.lblTotalDownloaded.Size = new System.Drawing.Size(95, 13);
+            this.lblTotalDownloaded.TabIndex = 15;
+            this.lblTotalDownloaded.Text = "Total downloaded:";
+            this.lblTotalDownloaded.Visible = false;
+            // 
+            // rdbMp3
+            // 
+            this.rdbMp3.AutoSize = true;
+            this.rdbMp3.Location = new System.Drawing.Point(179, 20);
+            this.rdbMp3.Name = "rdbMp3";
+            this.rdbMp3.Size = new System.Drawing.Size(45, 17);
+            this.rdbMp3.TabIndex = 16;
+            this.rdbMp3.Text = "mp3";
+            this.rdbMp3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 382);
+            this.Controls.Add(this.rdbMp3);
+            this.Controls.Add(this.lblTotalDownloaded);
+            this.Controls.Add(this.lblTotalFiles);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.chklstFiles);
+            this.Controls.Add(this.rdbPub);
+            this.Controls.Add(this.rdbPdf);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbFolder);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Web Files Download by  Antonio Sánchez";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.TextBox txbFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rdbPdf;
+        private System.Windows.Forms.RadioButton rdbPub;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckedListBox chklstFiles;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblTotalFiles;
+        private System.Windows.Forms.Label lblTotalDownloaded;
+        private System.Windows.Forms.RadioButton rdbMp3;
+    }
+}
+
---
+++ WebFilesDownload/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace WebFilesDownload
+{
+    public partial class Form1 : Form
+    {
+        private const String msgCaption = "Web Files Download";
+        private String extension;
+        private List<String> fileLinks = new List<String>();
+        private Int32 countFiles = 0;
+        private Double totalKbDownloaded = 0.00;
+        private Int64 totalBytesDownloaded = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetFileExtension();
+            txbFolder.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            btnDownload.Enabled = false;
+            lblTotalFiles.Text = "Total files: " + countFiles + " / " + fileLinks.Count;
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            DownloadFiles();
+        }
+
+        private void txbFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                String path = folderBrowserDialog1.SelectedPath;
+                txbFolder.Text = path;
+            }
+        }
+
+        private void DownloadFiles()
+        {
+            String filePath = txbFolder.Text;
+            if (String.IsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("Select the folder where you want to save your files!", msgCaption);
+            }
+            else
+            {
+                btnDownload.Text = "Downloading...";
+                btnDownload.Enabled = false;
+
+                foreach (String file in fileLinks)
+                {
+                    String fileName = GetFileName(file);
+
+
+                    if (fileName.Length > 0)
+                    {
+                        try
+                        {
+                            using (WebClient webClient = new WebClient())
+                            {
+                                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                                webClient.DownloadFileAsync(new Uri(file), filePath + "\\" + fileName.ToString());
+                            }
+
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error: {0}", ex.Message);
+                        }
+                    }
+                }
+
+                btnDownload.Text = "Download";
+                btnDownload.Enabled = true;
+            }
+
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            
+            progressBar1.Value = e.ProgressPercentage;
+            //lblStatus.Text = String.Format("Downloading: {0} of {1} kb", (e.BytesReceived / 1024), (e.TotalBytesToReceive / 1024));
+            //totalKbDownloaded += (e.TotalBytesToReceive / 1024);
+            //lblTotalDownloaded.Text = String.Format("Total downloaded: {0} kb", totalKbDownloaded);
+
+            lblStatus.Text = String.Format("Downloading: {0} of {1}", SizeSuffix(e.BytesReceived), SizeSuffix(e.TotalBytesToReceive));
+            totalBytesDownloaded += e.TotalBytesToReceive;
+            //lblTotalDownloaded.Text = String.Format("Total downloaded: {0}", SizeSuffix(totalBytesDownloaded));
+        }
+
+
+        private static readonly String[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+        private static String SizeSuffix(Int64 value)
+        {
+            if (value < 0) { return "-" + SizeSuffix(-value); }
+            if (value == 0) { return "0.0 bytes"; }
+
+            Int32 mag = (Int32)Math.Log(value, 1024);
+            Decimal adjustedSize = (Decimal)value / (1L << (mag * 10));
+
+            return String.Format("{0:n1} {1}", adjustedSize, SizeSuffixes[mag]);
+        }
+
+
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            if(countFiles < fileLinks.Count)
+                countFiles++;
+
+            progressBar1.Value = 0;
+            lblTotalFiles.Text = "Total files: " + countFiles + " / " + fileLinks.Count;
+        }
+
+        private String GetFileName(String file)
+        {
+            String result = file.Split('/').Last();
+
+            if (result.Contains("%20"))
+                result = result.Replace("%20", "_");
+            if (result.Contains("%ba"))
+                result = result.Replace("%ba", "");
+
+            return result;
+        }
+
+        private void GetFiles()
+        {
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    String sourceCode = wc.DownloadString(txbUrl.Text);
+
+                    // Clear lists
+                    chklstFiles.Items.Clear();
+                    fileLinks.Clear();
+
+                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                    doc.LoadHtml(sourceCode);
+
+                    HtmlNode node = doc.DocumentNode;
+                    HtmlNodeCollection nodes = node.SelectNodes("//a[@href]");
+
+                    List<String> links = new List<String>();
+                    foreach (HtmlNode item in nodes)
+                    {
+                        String link = item.Attributes["href"].Value;
+                        if (link.Contains(extension))
+                        {
+                            // If link starts with / then is absolute path
+                            if (link.StartsWith("/"))
+                            {
+                                String fileName = GetFileName(link);
+                                link = txbUrl.Text + fileName;
+                            }
+
+                            links.Add(link.Contains("http") ? link : txbUrl.Text + link);
+                        }
+                    }
+
+                    foreach (String file in links)
+                    {
+                        if (file.LastIndexOf(extension) != 1)
+                        {
+                            fileLinks.Add(file.ToString());
+
+                            String itemName = GetFileName(file);
+                            if (!String.IsNullOrEmpty(itemName))
+                                chklstFiles.Items.Add(itemName, CheckState.Checked);
+                        }
+                    }
+
+                    if (chklstFiles.Items.Count > 0)
+                        btnDownload.Enabled = true;
+                    else
+                    {
+                        btnDownload.Enabled = false;
+                        MessageBox.Show(String.Format("No {0} files were found!", extension), msgCaption);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, msgCaption);
+            }
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            GetFileExtension();
+        }
+
+        private void GetFileExtension()
+        {
+            if (rdbPdf.Checked)
+                extension = "." + rdbPdf.Text.ToLower();
+            else if(rdbPub.Checked)
+                extension = "." + rdbPub.Text.ToLower();
+            else
+                extension = "." + rdbMp3.Text.ToLower();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            GetFiles();
+        }
+
+        private void chklstFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CheckedListBox clb = (CheckedListBox)sender;
+            // file = (String)clb.SelectedItem;
+
+            // To can check the status of the item and remove the file if the status is uncheck 
+            Int32 index = clb.SelectedIndex;
+
+            try
+            {
+                if (clb.CheckedItems.Count < 0)
+                {
+                    btnDownload.Enabled = false;
+                }
+                else
+                {
+                    if (index != -1 && clb.GetItemCheckState(index) == CheckState.Unchecked)
+                    {
+                        fileLinks.RemoveAt(index);
+                        chklstFiles.Items.RemoveAt(index);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: {0}", ex.Message);
+            }
+            
+
+        }
+    }
+}
